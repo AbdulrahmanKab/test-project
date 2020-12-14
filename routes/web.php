d5fa7541efc8c45d16b3cef3c6af61871f6b6368
Abdulrahamn Kabar <kabar.abdullrahman@gmail.com>
2020-12-14 13:57:47 +0200

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@
 
 Auth::routes();
 
-Route::get('/','indexController@index');
-Route::get('/getusers','indexController@get');
-Route::post('/save','indexController@save');
-Route::post('/Upload/File','indexController@upload');
-Route::put('/user/update/{id}','indexController@updateUser');
+
+
+Route::group(['middleware'=>'auth'],function (){
+    Route::get('/','indexController@index');
+    Route::get('/getusers','indexController@get');
+    Route::post('/save','indexController@save');
+    Route::post('/Upload/File','indexController@upload');
+    Route::put('/user/update/{id}','indexController@updateUser');
+});
